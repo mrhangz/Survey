f31e4b4847089c5065d0f9950f9da8a513f44d1f
Teerawat Vanasapdamrong <mrhangz@Teerawats-MacBook-Pro.local>
2017-07-12 12:08:53 +0700

    Initial commit

---
+++ Survey/SurveyCollectionViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SurveyCollectionViewController.swift
+//  Survey
+//
+//  Created by Teerawat Vanasapdamrong on 7/12/2560 BE.
+//  Copyright © 2560 Teerawat Vanasapdamrong. All rights reserved.
+//
+
+import UIKit
+
+class SurverCollectionViewController: UICollectionViewController {
+    
+}
+
+extension SurverCollectionViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.size.width, height: collectionView.frame.size.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+}
