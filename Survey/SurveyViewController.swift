e34ef7877c4df4d15c6e76f727fda7c6428c69b7
Teerawat Vanasapdamrong <mrhangz@Teerawats-MacBook-Pro.local>
2017-07-13 14:30:35 +0700

    implement refresh feature

--- Survey/SurveyViewController.swift
+++ Survey/SurveyViewController.swift
@@ -20,10 +20,17 @@ class SurveyViewController: UIViewController {
     var surveys: [Survey] = []
     
     override func viewDidLoad() {
-        KVLoading.show()
         pageControl.transform = CGAffineTransform(rotationAngle: .pi / 2)
         pageControl.frame = CGRect(x: self.view.frame.size.width - pageControl.frame.size.width, y: 0, width: pageControl.frame.size.width, height: self.collectionView.frame.size.height)
-        
+        getSurveys()
+    }
+    
+    @IBAction func refresh(sender: UIBarButtonItem) {
+        getSurveys()
+    }
+    
+    func getSurveys() {
+        KVLoading.show()
         NetworkManager(token: "36dece969c2c0ee12ffea0f63ad15e7e237e76434ca0f7c2bcf2e56a821d2011").getSurveys { (surveys, error) in
             if surveys != nil {
                 self.surveys = surveys!
@@ -32,6 +39,7 @@ class SurveyViewController: UIViewController {
             } else {
                 print("\(error!.localizedDescription)")
             }
+            self.collectionView.scrollToItem(at: IndexPath(row: 0, section: 0), at: .top, animated: false)
             KVLoading.hide()
         }
     }
